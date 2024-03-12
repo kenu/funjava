71fd9decf44d935e0087e0a505d3c047e19b6a79
kenu <kenu@okjsp.pe.kr>
2024-03-13 03:10:13 +0900

    api

--- src/main/java/com/okdevtv/java/funjava/WordController.java
+++ src/main/java/com/okdevtv/java/funjava/WordController.java
@@ -2,7 +2,9 @@ package com.okdevtv.java.funjava;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class WordController {
@@ -11,4 +13,16 @@ public class WordController {
     model.addAttribute("words", Words.getString());
     return "index";
   }
+}
+
+@CrossOrigin(origins = "*")
+@RestController
+class WordRestController {
+  @GetMapping("/api/words")
+  public Word word() {
+    return new Word(Words.getString());
+  }
+}
+
+record Word(String content) {
 }
